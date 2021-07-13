344b9cfaabfa730768632bf133491c4176b3ec04
Ollie Prentice <osprentice@gmail.com>
2021-07-13 17:31:58 +0100

    facebook code change

--- pages/_document.js
+++ pages/_document.js
@@ -15,28 +15,24 @@ class MyDocument extends Document {
           <Main />
 
           <div id="fb-root"></div>
-          <div id="fb-customer-chat" class="fb-customerchat"></div>
+          <div id="fb-customer-chat" class="fb-customerchat" data-page_id="104181658543041"></div>
 
           <NextScript />
           <script dangerouslySetInnerHTML={{
-                        __html: `var chatbox = document.getElementById('fb-customer-chat');
-                        chatbox.setAttribute("page_id", "104181658543041");
-                        chatbox.setAttribute("attribution", "biz_inbox");
-                    
-                        window.fbAsyncInit = function() {
-                          FB.init({
-                            xfbml            : true,
-                            version          : 'v11.0'
-                          });
-                        };
-                    
-                        (function(d, s, id) {
-                          var js, fjs = d.getElementsByTagName(s)[0];
-                          if (d.getElementById(id)) return;
-                          js = d.createElement(s); js.id = id;
-                          js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
-                          fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script', 'facebook-jssdk'));`
+                        __html: `window.fbAsyncInit = function() {
+                            FB.init({
+                                xfbml: true,
+                                version: 'v10.0'
+                                });
+                            };
+
+                            (function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                            fjs.parentNode.insertBefore(js, fjs);
+                           }(document, 'script', 'facebook-jssdk'))`
                     }} />
         </body>
       </Html>
